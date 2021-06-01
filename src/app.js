65d480414c6c727aec8417ac5066e65c08759d5c
VIK2395 <vik.chupak@gmail.com>
2021-06-01 15:37:52 +0300

    cookieSessionMiddleware secure dissabled

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === 'development') {
 const cookieSessionMiddleware = cookieSession({
   maxAge: 60 * 60 * 1000, // 1h in miliseconds
   keys: [process.env.COOKIE_KEY],
-  secure: true,
+  // secure: true,
   httpOnly: true,
 });
 
