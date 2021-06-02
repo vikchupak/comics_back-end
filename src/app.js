e9f4149ed86d975fefd5851323e483f0c8690476
VIK2395 <vik.chupak@gmail.com>
2021-06-02 16:57:56 +0300

    deploy

--- src/app.js
+++ src/app.js
@@ -46,9 +46,9 @@ if (process.env.NODE_ENV === 'development') {
 const cookieSessionMiddleware = cookieSession({
   maxAge: 60 * 60 * 1000, // 1h in miliseconds
   keys: [process.env.COOKIE_KEY],
-  sameSite: 'strict',
+  sameSite: 'none',
   // domain: process.env.BACK_END_DOMAIN,
-  // secure: true,
+  secure: true,
   httpOnly: true,
 });
 
