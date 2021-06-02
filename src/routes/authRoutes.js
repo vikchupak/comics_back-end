197ee41e1b562add63ae89b1845dff3a74203ace
VIK2395 <vik.chupak@gmail.com>
2021-06-02 19:08:29 +0300

    deploy

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -26,8 +26,13 @@ router.get(
   '/google/callback',
   passport.authenticate('google', { failireRedirect: '/auth/login' }),
   (req, res) => {
-    // res.end();
-    // console.log('res: ', res);
+    // res.cookie('jwt', token, {
+    //   httpOnly: true,
+    //   maxAge: 1000 * 60 * 60 * 24 * 365, // 1year in milliseconds // or null possible
+    //   sameSite: 'none',
+    //   secure: true,
+    // });
+    console.log('req: ', req);
     // this redirect makes the magic!!!
     // it sets cookies on port:3000, not 5000!!!
     res.redirect(process.env.FRONT_END_DOMAIN);
